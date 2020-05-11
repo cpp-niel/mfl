a4a05176b21bce6b560d4cafcefb684fff89d87c
Niel <cpp.niel@gmail.com>
2020-05-11 14:55:41 +0200

    ICE workaround

--- src/noad/overline.cpp
+++ src/noad/overline.cpp
@@ -8,8 +8,13 @@ namespace mfl
 {
     hlist overline_to_hlist(const settings s, const bool is_cramped, const overline& ol)
     {
-        return make_line(s, is_cramped, ol,
-                         {.gap = overline_gap(s), .padding = overline_padding(s), .thickness = overline_thickness(s)},
-                         make_up_vbox);
+        if (ol.noads.empty()) return {};
+
+        auto content = clean_box(s, is_cramped, ol.noads);
+        const auto w = content.dims.width;
+
+        auto l = make_vlist(kern{.size = overline_gap(s)}, rule{.width = w, .height = overline_thickness(s), .depth = 0},
+                            kern{.size = overline_padding(s)});
+        return make_hlist(make_up_vbox(w, std::move(content), std::move(l)));
     }
 }
--- src/noad/underline.cpp
+++ src/noad/underline.cpp
@@ -8,9 +8,13 @@ namespace mfl
 {
     hlist underline_to_hlist(const settings s, const bool is_cramped, const underline& ul)
     {
-        return make_line(
-            s, is_cramped, ul,
-            {.gap = underline_gap(s), .padding = underline_padding(s), .thickness = underline_thickness(s)},
-            make_down_vbox);
+        if (ul.noads.empty()) return {};
+
+        auto content = clean_box(s, is_cramped, ul.noads);
+        const auto w = content.dims.width;
+
+        auto l = make_vlist(kern{.size = underline_gap(s)}, rule{.width = w, .height = underline_thickness(s), .depth = 0},
+                            kern{.size = underline_padding(s)});
+        return make_hlist(make_down_vbox(w, std::move(content), std::move(l)));
     }
 }
