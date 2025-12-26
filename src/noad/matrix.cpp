12fe09627e3a82a1af4bf3447a5edd9c329a2128
Niel <cpp.niel@gmail.com>
2025-12-26 11:26:14 +0100

    Add matrix parsing
    Matrices are parsed into a `matrix` struct that contains the matrix rows.
    Tests ensure that simple and more complex matrices are being parsed correctly
    as well as triggering the potential "missing closing brace error".

---
+++ src/noad/matrix.cpp
@@ -0,0 +1,9 @@
+#include "noad/matrix.hpp"
+
+#include "node/hlist.hpp"
+#include "settings.hpp"
+
+namespace mfl
+{
+    hlist matrix_to_hlist(const settings, const cramping, const matrix&) { return {}; }
+}
